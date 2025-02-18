50b1a1807263a2f92d9b293c97ce75107adeebb6
BodaciousTea <tebodacious@gmail.com>
2025-02-17 22:54:18 -0600

    import hashRouther instead of browserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,14 @@ import { Buffer } from "buffer";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // import BrowserRouter
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const basename = import.meta.env.PROD ? "/delivery-cms-frontend" : "/";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
