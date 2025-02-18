16e8129cd738466845273bde1d1892bf521ef084
BodaciousTea <tebodacious@gmail.com>
2025-02-17 22:46:55 -0600

    Fix nested router issue and update routing for production

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard"; // Use default import
 import LoginPage from "./components/LoginPage";
 import AdminLogin from "./pages/Admin/AdminLogin";
@@ -9,34 +9,32 @@ import { PrivateRoute } from "./components/PrivateRoute";
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <main className="container">
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            {/* Admin routes */}
-            <Route path="/admin/login" element={<AdminLogin />} />
-            <Route
-              path="/admin"
-              element={
-                <AdminPrivateRoute>
-                  <AdminDashboard />
-                </AdminPrivateRoute>
-              }
-            />
-            {/* Other routes */}
-            <Route
-              path="/"
-              element={
-                <PrivateRoute>
-                  <Dashboard />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="app">
+      <main className="container">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          {/* Admin routes */}
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route
+            path="/admin"
+            element={
+              <AdminPrivateRoute>
+                <AdminDashboard />
+              </AdminPrivateRoute>
+            }
+          />
+          {/* Other routes */}
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import { Buffer } from "buffer";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // import BrowserRouter
 import App from "./App";
 import "./index.css";
 
+const basename = import.meta.env.PROD ? "/delivery-cms-frontend" : "/";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
