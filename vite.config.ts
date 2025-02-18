7aba730118dc36af9ed57771fd8008989965316c
BodaciousTea <tebodacious@gmail.com>
2025-02-17 20:53:38 -0600

    deployment fix1

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,30 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
-export default defineConfig({
-  base: "/delivery-cms-frontend/",
-  plugins: [
-    react(),
-    NodeGlobalsPolyfillPlugin({
-      buffer: true,
-      process: true,
-    }),
-  ],
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
+export default defineConfig(({ mode }) => {
+  return {
+    // Use root path in dev, subpath in production
+    base: mode === "production" ? "/delivery-cms-frontend/" : "/",
+
+    plugins: [
+      react(),
+      NodeGlobalsPolyfillPlugin({
+        buffer: true,
+        process: true,
+      }),
+    ],
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: "globalThis",
+        },
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            buffer: true,
+            process: true,
+          }),
+        ],
       },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-          process: true,
-        }),
-      ],
     },
-  },
+  };
 });
